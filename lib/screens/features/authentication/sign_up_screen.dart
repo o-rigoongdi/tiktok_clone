d91dc85047a1838843270de57b0011ddd0b8e190
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2023-01-16 12:02:42 +0900

    4.1 Login Screen

--- lib/screens/features/authentication/sign_up_screen.dart
+++ lib/screens/features/authentication/sign_up_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/features/authentication/login_screen.dart';
+import 'package:tiktok_clone/screens/features/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
+
+  void onLoginTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +40,9 @@ class SignUpScreen extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
+              Gaps.v40,
+              AuthButton(text: 'Use phone or email'),
+              AuthButton(text: 'Continue with Apple'),
             ],
           ),
         ),
@@ -44,11 +57,14 @@ class SignUpScreen extends StatelessWidget {
             children: [
               const Text('Already have an account?'),
               Gaps.h5,
-              Text(
-                'Log in',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.w600,
+              GestureDetector(
+                onTap: () => onLoginTap(context),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ],
